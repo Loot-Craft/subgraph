1d4e9b38e2346c6451b85b674623ae26ef7342d3
Joseph Bedminster <joseph.bedminster@gmail.com>
2022-08-18 21:45:35 +0200

    0.0.2

--- tests/token.test.ts
+++ tests/token.test.ts
@@ -7,10 +7,10 @@ import {
   afterAll
 } from "matchstick-as/assembly/index"
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { ExampleEntity } from "../generated/schema"
-import { Approval } from "../generated/LootCraftBattle/LootCraftBattle"
-import { handleApproval } from "../src/loot-craft-battle"
-import { createApprovalEvent } from "./loot-craft-battle-utils"
+import { Approval } from "../generated/schema"
+import { Approval as ApprovalEvent } from "../generated/Token/Token"
+import { handleApproval } from "../src/token"
+import { createApprovalEvent } from "./token-utils"
 
 // Tests structure (matchstick-as >=0.5.0)
 // https://thegraph.com/docs/en/developer/matchstick/#tests-structure-0-5-0
@@ -21,8 +21,8 @@ describe("Describe entity assertions", () => {
     let approved = Address.fromString(
       "0x0000000000000000000000000000000000000001"
     )
-    let ennemyId = BigInt.fromI32(234)
-    let newApprovalEvent = createApprovalEvent(owner, approved, ennemyId)
+    let tokenId = BigInt.fromI32(234)
+    let newApprovalEvent = createApprovalEvent(owner, approved, tokenId)
     handleApproval(newApprovalEvent)
   })
 
@@ -33,26 +33,26 @@ describe("Describe entity assertions", () => {
   // For more test scenarios, see:
   // https://thegraph.com/docs/en/developer/matchstick/#write-a-unit-test
 
-  test("ExampleEntity created and stored", () => {
-    assert.entityCount("ExampleEntity", 1)
+  test("Approval created and stored", () => {
+    assert.entityCount("Approval", 1)
 
     // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "Approval",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "owner",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "Approval",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
       "approved",
       "0x0000000000000000000000000000000000000001"
     )
     assert.fieldEquals(
-      "ExampleEntity",
-      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
-      "ennemyId",
+      "Approval",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a-1",
+      "tokenId",
       "234"
     )
 
